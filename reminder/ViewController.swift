cc292924ea6dfbf7aaef44143f40afbb9a808ea8
Vlad <vecherkovsky2003@mail.ru>
2020-07-12 15:21:11 +0300

    checking the operation of notifications and changing the background to purple

--- reminder/ViewController.swift
+++ reminder/ViewController.swift
@@ -7,14 +7,38 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let center = UNUserNotificationCenter.current()
+        
+        center.requestAuthorization(options: [.alert, .sound]) { (granted, error) in
+            
+        }
+        
+        let content = UNMutableNotificationContent()
+        content.title = "Notification"
+        content.body = "Text"
+        
+        let date = Date().addingTimeInterval(10)
+        
+        let dateComp = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: date)
+        
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComp, repeats: false)
+        
+        let uuidString = UUID().uuidString
+        let request = UNNotificationRequest(identifier: uuidString, content: content, trigger: trigger)
+        
+        center.add(request) { (errpr) in
+            // Check the error 
+        }
     }
-
+    
 
 }
 
